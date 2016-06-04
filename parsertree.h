13d0b9589cbaa610e647d4809da45952f62c54d8
JackWang822 <wang_kejie@foxmail.com>
2016-06-05 01:25:59 +0800

    update the lex input file with adding system value

--- parsertree.h
+++ parsertree.h
@@ -5,7 +5,7 @@
 #include <exception>
 #include <initializer_list>
 
-YYSTYPE newTreeNode(const initialzer_list<YYSTYPE> &children,
+YYSTYPE newTreeNode(const std::initialzer_list<YYSTYPE> &children,
                     const NodeType &nodeType,
                     const StmtType &stmtType,
                     const ExpType &expType);
