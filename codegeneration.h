8b3d9c8c7c21575b182020fc6f72895cfe06a8cd
zncanke <ckzheng0320@outlook.com>
2016-06-18 07:47:22 +0800

    kunsile

--- codegeneration.h
+++ codegeneration.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include "symtable.h"
 using namespace std;
 
-void generateCode(vector<Quad> inst);
+void generateCode(vector<Quad*> inst);
